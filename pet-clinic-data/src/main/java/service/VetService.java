9f58f918aad646eb0e34102e1c6a22a83d143abd
William1099 <williamliam1099@gmail.com>
2020-12-21 21:35:20 +0700

    Create interface for services, closes #5

---
+++ pet-clinic-data/src/main/java/service/VetService.java
@@ -0,0 +1,16 @@
+package service;
+
+
+
+import model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet Vet);
+
+    Set<Vet> findAll();
+}
