5edb8d3ca9579382cf4d711f97ef3d193a5ccc86
William1099 <williamliam1099@gmail.com>
2020-12-21 15:24:47 +0700

    refactor to multimodule maven project, closes #1

--- pet-clinic-data/src/main/java/model/Pet.java
+++ pet-clinic-data/src/main/java/model/Pet.java
@@ -1,4 +1,4 @@
-package com.springframework.petclinic.model;
+package model;
 
 import java.time.LocalDate;
 
--- pet-clinic-data/src/main/java/model/PetType.java
+++ pet-clinic-data/src/main/java/model/PetType.java
@@ -1,4 +1,4 @@
-package com.springframework.petclinic.model;
+package model;
 
 public class PetType {
 
